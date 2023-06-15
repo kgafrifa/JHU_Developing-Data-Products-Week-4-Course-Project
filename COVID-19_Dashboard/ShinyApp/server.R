a16d0565cd801479855148ce570165d9b7a1da1b
kgafrifa <46286843+kgafrifa@users.noreply.github.com>
2023-06-15 16:08:46 -0400

    Add files via upload

---
+++ COVID-19_Dashboard/ShinyApp/server.R
@@ -0,0 +1,30 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+# Define server logic
+server <- function(input, output) {
+  output$covid19plot <- renderPlotly({
+    if (!is.null(input$country)) {
+      x <- covid19(
+        country = input$country,
+        level = input$level,
+        start = input$date[1],
+        end = input$date[2]
+      )
+      color <- paste0("administrative_area_level_", input$level)
+      plot_ly(x = x[["date"]],
+              y = x[[input$type]],
+              color = x[[color]],
+              type = 'scatter',
+              mode = 'lines')
+      
+    }
+  })
+  
+}
